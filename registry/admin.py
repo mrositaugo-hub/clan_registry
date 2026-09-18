d7de51e01d3597bda4b4fc8507535fd236b1c693
Osita Wisdom <mrositaugo@gmail.com>
2026-09-19 00:18:20 +0100

    Register Registry, LifeEvent, MarriageDetails, and DivorceDetails models in admin

--- registry/admin.py
+++ registry/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
+from .models import Registry, LifeEvent, MarriageDetails, DivorceDetails
 
-# Register your models here.
+
+@admin.register(Registry)
+class RegistryAdmin(admin.ModelAdmin):
+    list_display = (
+        "aut_id",
+        "surname",
+        "firstname",
+        "family_root",
+        "gender",
+        "marital_status",
+        "phone_number",
+    )
+    list_filter = ("family_root", "gender", "marital_status")
+    search_fields = (
+        "aut_id",
+        "surname",
+        "firstname",
+        "phone_number",
+        "family_name",
+    )
+    readonly_fields = ("aut_id", "aut_reg_date")
+
+
+@admin.register(LifeEvent)
+class LifeEventAdmin(admin.ModelAdmin):
+    list_display = ("member", "event_type", "event_date", "event_location")
+    list_filter = ("event_type", "event_date")
+    search_fields = (
+        "member__surname",
+        "member__firstname",
+        "member__aut_id",
+        "event_location",
+    )
+
+
+@admin.register(MarriageDetails)
+class MarriageDetailsAdmin(admin.ModelAdmin):
+    list_display = ("member", "spouse_full_name", "date_of_marriage")
+    search_fields = (
+        "member__surname",
+        "member__firstname",
+        "spouse_full_name",
+    )
+
+
+@admin.register(DivorceDetails)
+class DivorceDetailsAdmin(admin.ModelAdmin):
+    list_display = ("marriage", "date_of_divorce")
+    search_fields = (
+        "marriage__member__surname",
+        "marriage__member__firstname",
+        "marriage__spouse_full_name",
+    )
